1dfa30e5b8aa1e739584df0f4e98f9fc88b65ad2
Patrick <patrickalmenezes@gmail.com>
2026-04-23 19:34:37 -0300

    skills component

--- src/app/components/page/page.ts
+++ src/app/components/page/page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Wellcome} from '../wellcome/wellcome';
 import {Header} from'../header/header';
+import { Skills } from '../skills/skills';
 @Component({
   selector: 'app-page',
-  imports: [Wellcome,Header],
+  imports: [Wellcome,Header,Skills],
   templateUrl: './page.html',
   styleUrl: './page.css',
 })
