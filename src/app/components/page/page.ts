bd4a8530bfa5600b70f1401dbadaa7f3f153f04a
Patrick <patrickalmenezes@gmail.com>
2026-04-25 17:42:22 -0300

    layout basico concluido

--- src/app/components/page/page.ts
+++ src/app/components/page/page.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 import { Wellcome} from '../wellcome/wellcome';
 import {Header} from'../header/header';
 import { Skills } from '../skills/skills';
+import { Projects } from '../projects/projects';
+import { Contact } from '../contact/contact';
+import { Footer } from '../footer/footer';
+
 @Component({
   selector: 'app-page',
-  imports: [Wellcome,Header,Skills],
+  imports: [Wellcome,Header,Skills,Projects,Contact,Footer],
   templateUrl: './page.html',
   styleUrl: './page.css',
 })
