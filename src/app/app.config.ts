8b5ab0dcbde94ab159c160d43f017e58040d3745
Patrick <patrickalmenezes@gmail.com>
2026-04-26 18:53:53 -0300

    Small adjustments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'; // Verifique se precisa do provideBrowserGlobalErrorListeners ou se foi erro de digitação
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    // provideZoneChangeDetection({ eventCoalescing: true }), // Comum no Angular 18/19+
+    
+    provideRouter(
+      routes, 
+      withInMemoryScrolling({ 
+        anchorScrolling: 'enabled', 
+        scrollPositionRestoration: 'enabled' 
+      })
+    ), // <--- A vírgula aqui é essencial e o withInMemoryScrolling fica dentro dos parênteses do provideRouter
+
+    provideClientHydration(withEventReplay())
   ]
 };
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,15 +1,22 @@
 import { Component,signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
+
+
+
 export class Header {
   isMenuOpen = signal(false);
 
   toggleMenu() {
     this.isMenuOpen.update(value => !value);
   }
+
+
+
 }
